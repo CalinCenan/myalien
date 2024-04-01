84c861a5e45b5e2b9a16ede60a875099660f2336
calin.cenan <calin.cenan@cs.utcluj.ro>
2024-04-01 10:47:41 +0300

    Setting Working!

--- alien.py
+++ alien.py
@@ -11,8 +11,6 @@ class Alien:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien")
-        # Set the background color.
-        self.bg_color = (230, 230, 230)
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -21,7 +19,7 @@ class Alien:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 # Redraw the screen during each pass through the loop.
-                self.screen.fill(self.bg_color)
+                self.screen.fill(self.settings.bg_color)
                 # Make the most recently drawn screen visible.
                 pygame.display.flip()
 
